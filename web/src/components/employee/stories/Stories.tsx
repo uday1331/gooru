f0fb32a39602dfb17d4d8afc5e1c6f90ac183e23
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-07-05 00:26:21 +0800

    Add search bar (#6)

--- web/src/components/employee/stories/Stories.tsx
+++ web/src/components/employee/stories/Stories.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Flex, Text, Button } from "@chakra-ui/core";
+import { MdArrowDropDown } from "react-icons/md";
 
 import { dummyStory1, dummyStory2 } from "../../../data";
 import { Story } from "../../../types/story";
+import { SearchBar } from "../../.";
 
 const listOfStories: Story[] = [dummyStory1, dummyStory2];
 
@@ -17,11 +19,22 @@ const StoriesHeading: React.FC = () => (
   </Flex>
 );
 
+const FilterBar: React.FC = () => (
+  <Flex direction="row" justify="space-between" w="100%" mt={8}>
+    <SearchBar placeholder="The story I made yesterday" />
+    <Flex direction="row" align="center">
+      <Text color="indigo">Sort by </Text>
+      <MdArrowDropDown />
+    </Flex>
+  </Flex>
+);
+
 export const Stories: React.FC = () => {
   console.log({ listOfStories });
   return (
-    <Flex py={3} px={5} width="100%">
+    <Flex py={3} px={5} width="100%" direction="column">
       <StoriesHeading />
+      <FilterBar />
     </Flex>
   );
 };
--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,3 +1,4 @@
 export { Employer } from "./employer";
 export { Employee } from "./employee";
 export { NavBar } from "./navbar";
+export { SearchBar } from "./search-bar";
