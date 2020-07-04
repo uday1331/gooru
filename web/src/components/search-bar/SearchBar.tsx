d9291ba5a745bcab4083c7b7ae658e8b4711de09
welvin21 <wewe2712@connect.hku.hk>
2020-07-05 00:23:46 +0800

    Add search bar

---
+++ web/src/components/search-bar/SearchBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { InputGroup, InputAddon, Input } from "@chakra-ui/core";
+import { MdSearch } from "react-icons/md";
+
+interface SearchBarProps {
+  placeholder: string;
+}
+
+export const SearchBar: React.FC<SearchBarProps> = ({ placeholder }) => (
+  <InputGroup size="md">
+    <InputAddon children={<MdSearch />} />
+    <Input rounded="0" placeholder={placeholder} width={300} />
+  </InputGroup>
+);
