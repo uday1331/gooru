743d23c5403ac6900c4580ba770a72a223113ea8
welvin21 <wewe2712@connect.hku.hk>
2020-07-04 18:39:59 +0800

    Add route to NavItem

--- web/src/components/employee/Employee.tsx
+++ web/src/components/employee/Employee.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Grid } from "@chakra-ui/core";
+import { Grid, Flex } from "@chakra-ui/core";
 import { MdDashboard, MdSettings, MdGamepad } from "react-icons/md";
+import { Route } from "react-router-dom";
 
 import { NavBar } from "../navbar";
 
@@ -8,14 +9,17 @@ const navItemList = [
   {
     name: "Dashboard",
     logo: <MdDashboard size={30} />,
+    component: <>Dashboard</>,
   },
   {
     name: "Stories",
     logo: <MdGamepad size={30} />,
+    component: <>Stories</>,
   },
   {
     name: "Settings",
     logo: <MdSettings size={30} />,
+    component: <>Settings</>,
   },
 ];
 
@@ -23,6 +27,13 @@ export const Employee: React.FC = () => {
   return (
     <Grid templateColumns="20% 80%">
       <NavBar navItemList={navItemList} />
+      <Flex>
+        {navItemList.map(({ name, component }, index) => (
+          <Route key={index} path={`/employee/${name.toLowerCase()}`}>
+            {component}
+          </Route>
+        ))}
+      </Flex>
     </Grid>
   );
 };
--- web/src/components/employer/Employer.tsx
+++ web/src/components/employer/Employer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Grid } from "@chakra-ui/core";
+import { Grid, Flex } from "@chakra-ui/core";
 import {
   MdDashboard,
   MdSettings,
   MdAssessment,
   MdGamepad,
 } from "react-icons/md";
+import { Route } from "react-router-dom";
 
 import { NavBar } from "../navbar";
 
@@ -13,18 +14,22 @@ const navItemList = [
   {
     name: "Dashboard",
     logo: <MdDashboard size={30} />,
+    component: <>Dashboard</>,
   },
   {
     name: "Stories",
     logo: <MdGamepad size={30} />,
+    component: <>Stories</>,
   },
   {
     name: "Track",
     logo: <MdAssessment size={30} />,
+    component: <>Track</>,
   },
   {
     name: "Settings",
     logo: <MdSettings size={30} />,
+    component: <>Settings</>,
   },
 ];
 
@@ -32,6 +37,13 @@ export const Employer: React.FC = () => {
   return (
     <Grid templateColumns="20% 80%">
       <NavBar navItemList={navItemList} />
+      <Flex>
+        {navItemList.map(({ name, component }, index) => (
+          <Route key={index} path={`/employer/${name.toLowerCase()}`}>
+            {component}
+          </Route>
+        ))}
+      </Flex>
     </Grid>
   );
 };
