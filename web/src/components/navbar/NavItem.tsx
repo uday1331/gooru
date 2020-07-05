1982e32bcd7240dc2fad86908f22acf5663bac49
Sean Chok <seanchok@gmail.com>
2020-07-05 15:48:04 +0800

    improve navbar ui with on hover colors (#10)

--- web/src/components/navbar/NavItem.tsx
+++ web/src/components/navbar/NavItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Text } from "@chakra-ui/core";
+import { Flex, PseudoBox, Text } from "@chakra-ui/core";
 import { Link, useLocation } from "react-router-dom";
 
 import { NavItemDetails } from "../template/Template";
@@ -15,20 +15,29 @@ export const NavItem: React.FC<NavItemProps> = ({
 
   return (
     <Link to={route} style={{ width: "100%" }}>
-      <Flex
-        direction="row"
-        justify="start"
-        align="center"
-        p={3}
-        pl={5}
+      <PseudoBox
         backgroundColor={pathname === route ? "indigo.500" : "white"}
         color={pathname === route ? "white" : "indigo.500"}
+        _hover={{
+          backgroundColor: "indigo.400",
+          color: "white",
+        }}
       >
-        <Flex p={0}>{logo}</Flex>
-        <Text fontSize="md" mx={2}>
-          {name}
-        </Text>
-      </Flex>
+        <Flex
+          direction="row"
+          justify="start"
+          align="center"
+          p={3}
+          pl={5}
+          color={"inherit"}
+          backgroundColor={"inherit"}
+        >
+          <Flex p={0}>{logo}</Flex>
+          <Text fontSize="md" mx={2}>
+            {name}
+          </Text>
+        </Flex>
+      </PseudoBox>
     </Link>
   );
 };
