cb2da482f6e3266295cbaa2fdbc3d44992d28f58
Ahsan Syed <40141811+ahsan8244@users.noreply.github.com>
2020-07-05 19:13:19 +0800

    adds centering and title to create story (#15)

--- web/src/components/createStory/CreateStoryTemplate.tsx
+++ web/src/components/createStory/CreateStoryTemplate.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Flex } from "@chakra-ui/core";
+import { Grid, Text, Flex, Box } from "@chakra-ui/core";
 import { ScenesSidebar } from "./ScenesSidebar";
 
 import { Story } from "../../types/story";
@@ -13,7 +13,20 @@ export const CreateStoryTemplate: React.FC<Props> = ({ children, story }) => {
   return (
     <Grid templateColumns="20% 80%">
       <ScenesSidebar story={story} />
-      <Flex>{children}</Flex>
+      <Grid templateRows="10% 90%" p={3} pl={5}>
+        <Text fontSize="3xl" p={0}>
+          {story.title}
+          <span style={{ color: "red" }}>.</span>
+        </Text>
+        <Flex
+          border="1px solid #111D5E"
+          align="center"
+          justify="center"
+          height="100%"
+        >
+          {children}
+        </Flex>
+      </Grid>
     </Grid>
   );
 };
--- web/src/components/createStory/EmptyStory.tsx
+++ web/src/components/createStory/EmptyStory.tsx
@@ -1,15 +1,6 @@
 import React from "react";
-import { Flex } from "@chakra-ui/core";
+import { Box } from "@chakra-ui/core";
 
 export const EmptyStory = () => {
-  return (
-    <Flex
-      width="100%"
-      height="100%"
-      alignItems="center"
-      justifyContent="center"
-    >
-      Ain’t no story without a scene, start by adding a new scene
-    </Flex>
-  );
+  return <Box>Ain’t no story without a scene, start by adding a new scene</Box>;
 };
