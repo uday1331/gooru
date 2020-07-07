02a5f0950f5e3b38f5f1fd37fae4d970b720bbb2
Sean Chok <seanchok@gmail.com>
2020-07-07 23:52:12 +0800

    build question scene (#33)

--- web/src/data/scenes.ts
+++ web/src/data/scenes.ts
@@ -18,6 +18,14 @@ const dummyScene2: Scene = {
     url:
       "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE2uCzt?ver=eb50&q=90&m=2&h=768&w=1024&b=%23FFFFFFFF&aim=true",
   },
+  question: "How should you deal with an angry customer?",
+  options: [
+    { id: 0, option: "fight them" },
+    { id: 1, option: "bring the problem to your manager" },
+    { id: 2, option: "patiently talk to them, and understand the problem" },
+    { id: 3, option: "leave them alone" },
+  ],
+  correctOption: 2,
 };
 
 export { dummyScene1, dummyScene2 };
