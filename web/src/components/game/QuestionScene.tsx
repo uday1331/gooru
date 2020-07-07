3bd0413a5113c24d00b60f5ef381a2e8f2a04364
seanchok <seanchok@gmail.com>
2020-07-07 23:51:02 +0800

    build question scene

--- web/src/components/game/QuestionScene.tsx
+++ web/src/components/game/QuestionScene.tsx
@@ -1,25 +1,94 @@
-import React, { FC } from "react";
-import { Scene } from "../../types/scene";
-import { Flex, Text } from "@chakra-ui/core";
+import React, { FC, useCallback, useState } from "react";
+import { ResourceType, Scene } from "../../types/scene";
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/core";
+import { Check } from "react-feather";
+import { SvgImage } from "../reusables";
 
 interface QuestionScene {
   scene: Scene;
 }
 
 export const QuestionScene: FC<QuestionScene> = ({ scene }) => {
-  const { title } = scene;
+  const { title, resource, question, options, correctOption } = scene;
+
+  const [height, setHeight] = useState<number>(489);
+  const box = useCallback(
+    node => setHeight(node.getBoundingClientRect().height),
+    []
+  );
+
+  const [selectedOption, setSelectedOption] = useState<number>(-1);
 
   return (
     <Flex
       h={"100%"}
-      my={4}
+      mt={4}
       direction={"column"}
       alignItems={"center"}
       justifyContent={"center"}
     >
-      <Text fontSize={"xl"} my={2}>
+      <Text fontSize={"xl"} fontWeight={"bold"} color={"revolver.500"} my={2}>
         {title}
       </Text>
+      <Flex
+        ref={box}
+        height={"100%"}
+        width={"100%"}
+        position={"relative"}
+        direction={"column"}
+        justifyContent={"flex-end"}
+      >
+        {resource?.type === ResourceType.IMAGE && (
+          <SvgImage
+            id={"scene"}
+            image={resource.url}
+            style={{
+              height,
+              width: "100%",
+              position: "absolute",
+              top: 0,
+              left: 0,
+            }}
+          />
+        )}
+        <Box
+          zIndex={10}
+          backgroundColor={"revolver.500"}
+          style={{ opacity: "80%" }}
+          color={"white"}
+          px={8}
+          py={4}
+        >
+          <Text fontSize={"lg"} fontWeight={"bold"}>
+            {question}
+          </Text>
+          <SimpleGrid columns={2} my={4} spacing={4}>
+            {options?.map(({ id, option }, index) => (
+              <Flex
+                key={index}
+                alignItems={"center"}
+                w={"fit-content"}
+                cursor={"pointer"}
+                onClick={() => setSelectedOption(id)}
+              >
+                <Flex
+                  h={"20px"}
+                  w={"20px"}
+                  backgroundColor={"white"}
+                  color={"amaranth.500"}
+                  borderRadius={"6px"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  mr={2}
+                >
+                  {selectedOption === id && <Check size={"16px"} />}
+                </Flex>
+                <Text>{option}</Text>
+              </Flex>
+            ))}
+          </SimpleGrid>
+        </Box>
+      </Flex>
     </Flex>
   );
 };
